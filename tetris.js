f1bf8a75a40b08e73365f5968eb7c2e7063f98c2
AdamJacobson <amartenj@gmail.com>
2017-02-03 13:51:58 -0800

    Board is now rendered without needing Row object. Grid now sourced from single object with random colors. Colors change randomly over time. Button starts and stops chaning colors.

--- tetris.js
+++ tetris.js
@@ -1,25 +1,11 @@
+const O = "yellow", L = "orange", J = "blue", T = "purple", I = "cyan", S = "lime", Z = "red";
+const colors = [O, L, T, J, I, S, Z];
+
 // Stateless functional component
 const Square = ({color}) => {
 	return (
 		<td className="gridSquare" style={{backgroundColor: color}}></td>
 	)
-}
-
-class Row extends React.Component {
-	constructor(props) {
-		super(props);
-	}
-	
-	render() {
-		const colors = Array(this.props.columns).fill("white");
-		const listItems = colors.map((color) =>
-  			<Square color={color}></Square>
-		);
-									 
-		return (
-			<tr>{listItems}</tr>
-		)
-	}
 }
 
 class Board extends React.Component {
@@ -28,18 +14,65 @@ class Board extends React.Component {
 	}
 	
 	render() {
-		const colors = Array(this.props.rows).fill(null);
-		const listItems = colors.map((color) =>
-  			<Row columns={this.props.columns}></Row>
-		);
-		
 		return (
-			<tbody>{listItems}</tbody>
+			<tbody>
+				{this.props.grid.map( (row, rindex) => 
+					<tr>
+						{row.map( (col, cindex) => 
+							<Square color={col} />
+						)}
+					</tr>
+				)}
+			</tbody>
 		)
 	}
 }
 
-ReactDOM.render(
-	<Board rows={20} columns={10}/>,
-	document.getElementById("board")
-)
+var makeGrid = function(width, height) {
+	var grid = [];
+	for (var i = 0; i < height; i++){
+  		grid[i] = [];
+  		for(var k = 0; k < width; k++) {
+    		grid[i][k] = colors[Math.floor(Math.random() * colors.length)];
+  		}
+	}
+	return grid;
+}
+
+var doStuff = function() {
+	var grid = makeGrid(10, 20);
+
+	ReactDOM.render(
+		<Board grid={grid} />,
+		document.getElementById("board")
+	)
+}
+
+function Game() {
+	var gameInterval;
+	var started = false;
+	
+	this.stopStart = function() {
+		if (started) {
+			stop();
+		} else {
+			start();
+		}
+	};
+	
+	var stop = function() {
+		clearInterval(gameInterval);
+		started = false;
+	};
+	
+	var start = function() {
+		gameInterval = setInterval(function(){doStuff();}, 1000)
+		started = true;
+	};
+	
+	return {
+		stopStart: this.stopStart
+	};
+}
+
+var game = new Game();
